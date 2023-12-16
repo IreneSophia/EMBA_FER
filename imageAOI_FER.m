2ae23c75e054584e2d49aeda2b7980ec9c6355b8
IreneSophia <10planki@gmail.com>
2023-12-16 10:27:17 +0100

    Switched to colours for the AOI overlay to better distinguish them.

--- imageAOI_FER.m
+++ imageAOI_FER.m
@@ -1,3 +1,8 @@
+% This script takes 3D matrices containing areas of interest (eye, 
+% forehead, mouth and nose region) as well as the original image and 
+% creates an image overlayed with the AOIs to check for fit. 
+% (c) I.S. Plank, 10plankp@gmail.com
+
 % clear everything
 clearvars;
 
@@ -8,6 +13,13 @@ ls_files = dir("VidStimProcessed" + filesep + "*.mat");
 str_progress = '        |------------------------------------------------------------|';
 count        = 1;
 
+% create the colourmap
+cm = zeros(256,3);
+cm(208,:) = [230/255 159/255 0      ];
+cm(215,:) = [86/255  180/255 233/255];
+cm(220,:) = [0       158/255 115/255];
+cm(225,:) = [204/255 121/255 167/255];
+
 % loop through the list
 for j = 1:length(ls_files)
 
@@ -17,7 +29,12 @@ for j = 1:length(ls_files)
     % check if the folder already exists
     dir_vid = ['VidStimProcessed' filesep vid];
     if exist(dir_vid, 'dir') > 0
-        continue
+        if length(dir([dir_vid filesep '*.png'])) == 300
+            continue
+        end
+    else
+        % create the directory
+        mkdir(dir_vid);
     end
 
     % add to progress bar
@@ -30,9 +47,6 @@ for j = 1:length(ls_files)
     % load the data
     load(['VidStimProcessed' filesep ls_files(j).name]);
 
-    % create the directory
-    mkdir(dir_vid);
-
     % loop through the frames
     for i = 1:size(mtx_aois,3)
 
@@ -42,15 +56,13 @@ for j = 1:length(ls_files)
         clc;
         disp(str_progress)
 
-        % read in the picture ADJUST AFTER GETTING PICTURES!
-        img = imread(['VidStimProcessed' filesep 'AF01AF_000.jpg']);
+        % read in the picture
+        fl  = dir(sprintf('%s\\taskFER\\Stimuli%s%s*%03d.jpg',...
+            pwd, filesep, vid, i-1));
+        img = imread([fl.folder filesep fl.name]);
 
-        % set all ROI values to 1
-        mtx = mtx_aois(:,:,i);
-        mtx(mtx > 0) = 1;
-
-        % convert to 3 colours
-        mtx = repmat(im2uint8(mtx),[1 1 3]);
+        % different colours for the different regions of interest
+        mtx = im2uint8(ind2rgb(im2uint8(mtx_aois(:,:,i)),cm));
 
         % perform the blending
         blended = mtx*0.5 + img;
